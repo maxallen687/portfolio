e35a44c84cdb6107db4fedae97b8224d282ed7d3
Max Allen <maxallen03919@gmail.com>
2025-07-18 08:22:27 +0900

    Update navbar.tsx

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -152,7 +152,7 @@ export function Navbar() {
                       animate={{ opacity: 1, x: 0 }}
                       transition={{ duration: 0.5 }}
                     >
-                      Achyut Katiyar
+                      Max Allen
                     </motion.span>
                   </Link>
                 </div>
@@ -220,7 +220,7 @@ export function Navbar() {
                           href="/"
                           className="font-display text-2xl font-bold transition-colors hover:text-primary"
                         >
-                          Achyut Katiyar
+                          Max Allen
                         </Link>
                         <ThemeSwitch />
                       </div>
