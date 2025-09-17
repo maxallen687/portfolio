b2d5847ace60c205c19d0c01c29dd74c982d6233
Max Allen <maxallen03919@gmail.com>
2025-09-18 04:15:28 +0900

    Update metadata.ts

--- app/metadata.ts
+++ app/metadata.ts
@@ -3,12 +3,12 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: "Max Allen | Portfolio",
-  description: "Full Stack Developer & Machine Learning Enthusiast",
+  description: "Full Stack and Blockchain Developer & Machine Learning Enthusiast",
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/icon.png", type: "image/png" },
+      { url: "/icon1.png", type: "image/png" },
     ],
-    apple: { url: "/apple-touch-icon.png", type: "image/png" },
+    apple: { url: "/apple-touch-icon1.png", type: "image/png" },
   },
 };
